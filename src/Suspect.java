09355446e2c5a804b7eda456956ae0b47ce6c987
User <pereraniveka@gmail.com>
2025-07-14 21:11:48 +0530

    created all the classes

---
+++ src/Suspect.java
@@ -0,0 +1,13 @@
+public class Suspect {
+    private String name;
+    private String alibi;
+    private boolean isGuilty;
+    private String weapon;
+
+    public Suspect(String name, String alibi, boolean isGuilty, String weapon){
+        this.name= name;
+        this.alibi = alibi;
+        this.isGuilty = isGuilty;
+        this.weapon =weapon;
+    }
+}
