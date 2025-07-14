cce00282803e367dc1a76e5745203b17fd958424
User <pereraniveka@gmail.com>
2025-07-14 21:17:21 +0530

    created all the classes

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,35 @@
 public class Item {
+    private String name;
+    private String description;
+    private boolean isRedHerring;
+
+    public Item(String name, String description, boolean isRedHerring) {
+        this.name = name;
+        this.description = description;
+        this.isRedHerring = isRedHerring;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description + (isRedHerring ? "(might be misleading)" : "");
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isRedHerring() {
+        return isRedHerring;
+    }
+
+    public void setRedHerring(boolean redHerring) {
+        isRedHerring = redHerring;
+    }
 }
--- src/Suspect.java
+++ src/Suspect.java
@@ -10,4 +10,36 @@ public class Suspect {
         this.isGuilty = isGuilty;
         this.weapon =weapon;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAlibi() {
+        return alibi;
+    }
+
+    public void setAlibi(String alibi) {
+        this.alibi = alibi;
+    }
+
+    public boolean isGuilty() {
+        return isGuilty;
+    }
+
+    public void setGuilty(boolean guilty) {
+        isGuilty = guilty;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
 }
